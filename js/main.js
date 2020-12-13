c2825a7a626c3838ddbfc863026e682bee94e747
dayant-wood <dayant.wood@gmail.com>
2020-12-13 22:16:15 +0300

    create: started section reviews

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSwider = new Swiper('.hotel-slider', {
   // Optional parameters
     loop: true,
 
     // Navigation arrows
   navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 
   effect: "coverflow",
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+
+  
 
   keyboard: {
     enabled: true,
