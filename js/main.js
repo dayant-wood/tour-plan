6029b4adea06526ed168d2e34a10c7d83321c8b2
dayant-wood <dayant.wood@gmail.com>
2020-12-30 12:22:05 +0300

    create: created validation forms

--- js/main.js
+++ js/main.js
@@ -67,5 +67,30 @@ closeModalButton.on("click", closeModal);
       modalOverlay.removeClass("modal__overlay--visible");
       modalDialog.removeClass("modal__dialog--visible");
   }
+  })  
+
+  //обработка форм
+
+  $(".form").each(function() {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+    name: {
+      required: "Please, specify your name",
+      minlength: "Name should contain minimum 3 characters",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+     phone: {
+       required: "Please, specify your phone number",
+       minlength: "Please, enter the full number",
+     },
+  }
+      
+});
 })
+
+  $(".phone-input").mask("+7(000) 000-00-00");
 });
